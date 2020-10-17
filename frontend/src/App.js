6738577dc9a2221fe5c7e428a834aa4efa365c02
kwong <kirkwong33@gmail.com>
2020-10-17 16:00:16 -0700

    Frontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path='/' component={home} />
+					<Route exact path='/login' component={login} />
+					<Route exact path='/signup' component={signup} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
-
 export default App;
